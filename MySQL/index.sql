30336fbdd72946178cfc38708ad692ac08dc1a28
FLYFLU <1475750554@qq.com>
2019-07-05 15:48:31 +0800

    Update index.sql
    in和exists区别

--- MySQL/index.sql
+++ MySQL/index.sql
@@ -18,3 +18,20 @@ select id ,@rankno:=@rankno+1 from table1 ,(select @rankno:=0) r ;
 -- MYSQL 8.0
 SELECT id,rank () OVER (ORDER BY id) indexer FROM node_copy1;
 -- see also ,rank () OVER (PARTITION BY a,b ORDER BY b)
+
+--in和exists区别
+--区分in和exists主要是造成了驱动顺序的改变（这是性能变化的关键）
+--如果是exists，那么以外层表为驱动表，先被访问，如果是IN，那么先执行子查询。
+--所以IN适合于外表大而内表小的情况；EXISTS适合于外表小而内表大的情况
+
+--sql 优化，没有验证
+--原sql
+select colname ... from A表 where a.id not in (select b.id from B表)
+--优化后的sql
+select colname … from A表 Left join B表 on where a.id = b.id where b.id is null
+
+--分页优化，没有验证
+--原sql
+select id,name from product limit 866613, 20
+--优化sql
+select id,name from product where id> 866612 limit 20
